5a78477da41537a38b28296622da69625666b5a3
Joe McCann <joseph.isaac@gmail.com>
2012-09-18 08:13:50 -0500

    added button styling, refresh capabilities

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var request = require('request')
+  , jsdom = require('jsdom')
+
 /*
  * GET home page.
  */
@@ -7,38 +10,46 @@ exports.index = function(req, res){
 };
 
 exports.get_npm_count = function(req, res){
+  console.log('Get npm count...')
+  
   getNpmCount(res)
 };
 
 
 function getNpmCount(res){
-  // Fetch the page
-  var request = require('request')
-    , jsdom = require('jsdom')
+
+  console.log('Sending request to npmjs.org...')
     
-    request.get('http://npmjs.org', function(e,r,b){
-      if(e) return res.status(500).send('Something fuct up')
-      if(r.statusCode > 399) return res.status(r.statusCode).message("Something not right")
+  request.get('http://npmjs.org', function(e,r,b){
+    
+    console.log('Received npmjs.org response...')
+    if(e) return res.status(500).send('Something fuct up')
+    if(r.statusCode > 399) return res.status(r.statusCode).message("Something not right")
 
-      jsdom.env({
-        html: b,
-        scripts: [
-          'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'
-        ]
-      }, function(err, window){
-
-        var $ = window.jQuery
-
-        var count = parseInt( $('#index > p:first').text().replace('Total Packages: ', '') )
-        
-        console.log("Total number of modules:  " + count)
-        
-        res.json({totalModules: count})
-        
-        // https://github.com/tmpvar/jsdom/issues/226
-        // window.close() // required to stop leaking. 
-        
-      })  // end env callback for jsdom
+    jsdom.env({
+      html: b,
+      scripts: [
+        'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'
+      ]
+    }, function(err, window){
       
-    })
+      console.log('Scraping response...')
+      
+      var $ = window.jQuery
+
+      var count = parseInt( $('#index > p:first').text().replace('Total Packages: ', '') )
+      
+      console.log("Total number of modules:  " + count)
+      
+      res.json({totalModules: count})
+      
+      // https://github.com/tmpvar/jsdom/issues/226
+      process.nextTick(function(){
+        window.close() // required to stop leaking.
+        delete window
+      })  
+      
+    })  // end env callback for jsdom
+    
+  })
 }
